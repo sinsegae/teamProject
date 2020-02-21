15d4f0f1b9d67d4c600d55ce90cac06b0b46ea14
sinryuji <siler2474@gmail.com>
2020-02-21 17:37:00 +0900

    sinryuji test2

--- src/main/java/naruto/NarutoDao.java
+++ src/main/java/naruto/NarutoDao.java
@@ -4,6 +4,7 @@ public class NarutoDao {
 	
 	public void bunsin(String naruto) {
 		System.out.println(naruto + " : 그림자 분신술");
+		System.out.println(naruto + " : 나선환");
 	}
 
 	
--- src/main/java/naruto/NarutoMain.java
+++ src/main/java/naruto/NarutoMain.java
@@ -3,6 +3,7 @@ package naruto;
 public class NarutoMain {
 	public static void main(String[] args) {
 		NarutoDao nd = new NarutoDao();
+		nd.bunsin("나루토");
 
 	}
 	
