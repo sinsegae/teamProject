cf67d8dfde3885eeb35716c96fe132474a1273e9
sinsegae <abtlel@daum.net>
2020-02-21 17:29:01 +0900

    test

---
+++ src/main/java/naruto/NarutoDao.java
@@ -0,0 +1,10 @@
+package naruto;
+
+public class NarutoDao {
+	
+	public void bunsin(String naruto) {
+		System.out.println(naruto + " : 그림자 분신술");
+	}
+
+	
+}
---
+++ src/main/java/naruto/NarutoMain.java
@@ -0,0 +1,9 @@
+package naruto;
+
+public class NarutoMain {
+	public static void main(String[] args) {
+		NarutoDao nd = new NarutoDao();
+
+	}
+	
+}
