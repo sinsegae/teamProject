f757962fcfc583669882e81e864baf9fb85caea0
baebaee <wjstkdqo002@naver.com>
2020-02-21 17:54:35 +0900

    가마분타술

--- src/main/java/naruto/NarutoDao.java
+++ src/main/java/naruto/NarutoDao.java
@@ -6,6 +6,8 @@ public class NarutoDao {
 		System.out.println(naruto + " : 그림자 분신술");
 		System.out.println(naruto + " : 나선환");
 		System.out.println(naruto + " : 구미화 꼬리 4개");
+		System.out.println(naruto + " : 가마분타술");
+
 	}
 
 	
