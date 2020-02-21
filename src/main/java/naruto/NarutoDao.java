9482671958fac7132573894fc303c9b01b93e92c
baebaee <wjstkdqo002@naver.com>
2020-02-21 17:48:46 +0900

    구미화꼬리4개

--- src/main/java/naruto/NarutoDao.java
+++ src/main/java/naruto/NarutoDao.java
@@ -5,6 +5,7 @@ public class NarutoDao {
 	public void bunsin(String naruto) {
 		System.out.println(naruto + " : 그림자 분신술");
 		System.out.println(naruto + " : 나선환");
+		System.out.println(naruto + " : 구미화 꼬리 4개");
 	}
 
 	
