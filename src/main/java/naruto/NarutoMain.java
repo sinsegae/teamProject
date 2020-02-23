1a0aa5acb2f01df79095e29df37fd31b91b41618
baebae <wjstkdqo002@naver.com>
2020-02-23 18:11:02 +0900

    2020-02-23 요구사항

--- src/main/java/naruto/NarutoMain.java
+++ src/main/java/naruto/NarutoMain.java
@@ -3,7 +3,10 @@ package naruto;
 public class NarutoMain {
 	public static void main(String[] args) {
 		NarutoDao nd = new NarutoDao();
+		SbDao sd = new SbDao();
 		nd.bunsin("나루토");
+		System.out.println("====================SB요구사항=========================");
+		sd.SB("◎");
 
 	}
 	
---
+++ src/main/java/naruto/SbDao.java
@@ -0,0 +1,12 @@
+package naruto;
+
+public class SbDao {
+	public void SB(String SB){
+		System.out.println(SB+" 1. 액티비티, 가족여행, 힐링여행 등 테마메뉴를 만들어서 테마별로 볼 수 있는 기능");
+		System.out.println(SB+" 2. SNS에 태그된 횟수 카운팅해서 핫스팟인 곳 나타내기");
+		System.out.println(SB+" 3. 사이트에서 추천한 경로의 이동수단, 숙소, 밥집 등 미리결제를 할 시 전체 결제 내역을 한번에 볼 수 있는 기능");
+		System.out.println(SB+" 4. 모르는 사람들과 단체여행 할수 있게 신청하는 이벤트(ex:패키지여행, 게스트하우스)");
+		System.out.println(SB+" 5. 계절마다 추천해주는 여행지 코스, 페스티벌");
+		System.out.println(SB+" 6. 차가 없는 여행자들을 위한 전담 택시투어 또는 버스투어 또는 자전거 대여소 신청 기능");
+	}
+}
